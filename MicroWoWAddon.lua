c95074321cb5c0603638c95d62576b50d1e92eb6
microk2 <tdanalache@gmail.com>
2021-03-01 23:37:34 +0200

    Update

--- MicroWoWAddon.lua
+++ MicroWoWAddon.lua
@@ -1,17 +1,30 @@
-local DEBUG = false;
+local DEBUG = true;
 
 local AddonFrame = CreateFrame("Frame", "MicroWoWAddonFrame");
 AddonFrame:RegisterEvent("GOSSIP_SHOW");
 AddonFrame:RegisterEvent("QUEST_DETAIL");
+AddonFrame:RegisterEvent("QUEST_COMPLETE");
+AddonFrame:RegisterEvent("QUEST_PROGRESS");
 
 AddonFrame:SetScript("OnEvent", function(self, event, ...)
 	if (event == "GOSSIP_SHOW") 		then HandleGossipShow()
-	elseif (event == "QUEST_DETAIL") 	then HandleQuestDetail()
+	elseif 	(event == "QUEST_DETAIL") 	or 
+			(event == "QUEST_COMPLETE") or 
+			(event == "QUEST_PROGRESS") 
+			then ShowQuestID(QuestFrame, nil, "ANCHOR_RIGHT", 0, -32)
 	end
+end)
+
+QuestMapDetailsScrollFrame:HookScript("OnShow", function(self)
+	local questIndex = GetQuestLogSelection();
+	local questInfo = { GetQuestLogTitle(questIndex) };
+	local questID = questInfo[8];
+	ShowQuestID(self, questID, "ANCHOR_RIGHT", 28, 0)
 end)
 
 function HandleGossipShow()
 	local availableQuests = { GetGossipAvailableQuests() };
+	local activeQuests = { GetGossipActiveQuests() };
 	local i = 1;
 	local maxCharsPerLine = 52;
 	local increaseLineHeightBase = 10;
@@ -37,12 +50,41 @@ function HandleGossipShow()
 			end
 		end
 	end
+	
+	for key, val in pairs(activeQuests) do
+		if (key % 7 == 0) then
+			local questId = val;
+			local parentFrame = _G["GossipTitleButton" .. i + 1];
+			local parentFrameText = parentFrame:GetText();
+			if (parentFrameText ~= nil) then
+				local newText = '[' .. questId .. '] ' .. parentFrameText;
+				parentFrame:SetText(newText);
+
+				if (newText:len() > maxCharsPerLine) then
+					parentFrame:SetHeight(parentFrame:GetHeight() + increaseLineHeightBase);
+				end
+				
+				i=i+1
+				
+				if DEBUG then
+					print("-- DEBUG GOSSIP SHOW --");
+					print("Old text length: " .. parentFrameText:len());
+					print("New text length: " .. newText:len());
+				end
+			end
+		end
+	end
 end
 
-function HandleQuestDetail()
-	local tooltipFrame = _G["MicroQuestTooltip"] or CreateFrame("GameTooltip", "MicroQuestTooltip", QuestFrameDetailPanel, "GameTooltipTemplate");
-	tooltipFrame:SetOwner(QuestFrameDetailPanel, "ANCHOR_RIGHT", 0, -32);
+function ShowQuestID(p_ParentFrame, p_QuestID, p_Anchor, p_OffsetX, p_OffsetY)
+	local frameName = "MicroQuestTooltip" .. "." .. p_ParentFrame:GetName();
+	local tooltipFrame = _G[frameName] or CreateFrame("GameTooltip", frameName, p_ParentFrame, "GameTooltipTemplate");
+	local questID = p_QuestID;
+	if (questID == nil) then questID = GetQuestID(); end;
+	
+	tooltipFrame:SetOwner(p_ParentFrame, p_Anchor, p_OffsetX, p_OffsetY);
 	tooltipFrame:ClearLines();
-	tooltipFrame:AddDoubleLine("QuestID:", GetQuestID());
-	tooltipFrame:Show()
+	tooltipFrame:AddDoubleLine("QuestID:", questID);
+	tooltipFrame:Show();
 end
+
