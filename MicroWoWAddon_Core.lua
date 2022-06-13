8d5f30af45e25d507ab6f7fc519090473520c79d
microk2 <tdanalache@gmail.com>
2022-06-13 21:54:46 +0300

    Update input frame and fix indent

--- MicroWoWAddon_Core.lua
+++ MicroWoWAddon_Core.lua
@@ -4,35 +4,35 @@ MWA.Core.CreateQuestIDFrame = function (parentFrame, questId, anchor, offsetX, o
     local questFrameName = MWA.Defines.AddonName .. "." .. parentFrame:GetName()
     local tooltipFrame = _G[questFrameName] or CreateFrame("GameTooltip", questFrameName, parentFrame, "GameTooltipTemplate")
     tooltipFrame:SetOwner(parentFrame, anchor, offsetX, offsetY)
-	tooltipFrame:ClearLines()
-	tooltipFrame:AddDoubleLine("QuestID:", questId)
-	tooltipFrame:Show()
+    tooltipFrame:ClearLines()
+    tooltipFrame:AddDoubleLine("QuestID:", questId)
+    tooltipFrame:Show()
 
     local WoWHeadBtnName = questFrameName .. "." .. "WoWHeadBtn"
     local WowHeadBtn = _G[WoWHeadBtnName] or CreateFrame("Button", WoWHeadBtnName, parentFrame, "UIPanelButtonTemplate")
     WowHeadBtn:SetPoint("CENTER", tooltipFrame, "BOTTOM", 0, -20)
-	WowHeadBtn:SetWidth(tooltipFrame:GetWidth())
-	WowHeadBtn:SetHeight(30)
-	WowHeadBtn:SetText("WoWHead Link")
+    WowHeadBtn:SetWidth(tooltipFrame:GetWidth())
+    WowHeadBtn:SetHeight(30)
+    WowHeadBtn:SetText("WoWHead Link")
 
     WowHeadBtn:SetScript("OnClick", function(self) 
-       MWA.Core.CreateEditBoxFrame("WoWHead Quest Link", "https://www.wowhead.com/quest=" .. questId, 400, 200)
+        MWA.Core.CreateEditBoxFrame("WoWHead Quest Link", "https://www.wowhead.com/quest=" .. questId, 400, 200)
     end)
 end
 
 MWA.Core.CreateEditBoxFrame = function(title, text, sizeX, sizeY)
     local editBoxFrameName = MWA.Defines.AddonName .. "." .. "EditBoxFrame";
     local editBox = _G[editBoxFrameName] or CreateFrame("Frame", editBoxFrameName, UIParent, "DialogBoxFrame")
-	editBox:SetSize(sizeX, sizeY)
-	editBox:SetPoint("CENTER", UIParent, "CENTER")
+    editBox:SetSize(sizeX, sizeY)
+    editBox:SetPoint("CENTER", UIParent, "CENTER")
     editBox:SetBackdropBorderColor(0, 0.44, 0.87, 0.5)
     editBox:Show()
 
     local inputFieldName = editBox:GetName() .. "." .. "InputField"
     local inputFieldFrame = _G[inputFieldName] or CreateFrame("EditBox", inputFieldName, editBox, "InputBoxTemplate")
     inputFieldFrame:SetParent(editBox)
-    inputFieldFrame:SetSize(editBox:GetWidth() - 100, 100)
-	inputFieldFrame:SetPoint("CENTER", editBox)
+    inputFieldFrame:SetSize(editBox:GetWidth() - 50, 30)
+    inputFieldFrame:SetPoint("CENTER", editBox)
     inputFieldFrame:SetText(text)
     inputFieldFrame:SetFocus()
 
